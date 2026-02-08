b2b957b282864b84d2c254f27763827a9fe1fb74
Dylan Tarre <timecreepsby@gmail.com>
2026-02-07 20:18:20 -0800

    chore: sync all pending changes and add CF deploy workflow

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Site configuration - edit these values to update across the site
 export const siteConfig = {
-  githubUrl: 'https://github.com/buoy-design/buoy',
+  githubUrl: 'https://github.com/ahoybuoy/buoy',
   twitterUrl: 'https://twitter.com/buoy_dev',
   discordUrl: 'https://discord.gg/buoy',
   enterpriseEmail: 'enterprise@buoy.design',
